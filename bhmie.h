3a6d214307861a125b1733c97ff07602f4dc61d2
Christophe Pinte <christophe.pinte@univ-grenoble-alpes.fr>
2025-08-06 19:34:04 +1000

    Cleaning header file

--- bhmie.h
+++ bhmie.h
@@ -1,14 +1,10 @@
-// 1. Include guard to prevent multiple inclusions
-#ifndef BHMIE_H
-#define BHMIE_H
+#pragma once
 
 #include <vector>
 #include <complex>
 
-// 2. Namespace to prevent naming collisions
 namespace Mie {
 
-    // The result structure is now inside the Mie namespace
     struct Result {
         std::vector<std::complex<double>> s1;
         std::vector<std::complex<double>> s2;
@@ -18,9 +14,6 @@ namespace Mie {
         double g_sca;
     };
 
-    // The function declaration is also in the namespace
     Result bhmie(double x, const std::complex<double>& refrel, int nang);
 
-} // namespace Mie
-
-#endif // BHMIE_H
+}
